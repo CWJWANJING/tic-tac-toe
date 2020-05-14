545495eb45302536e4a347c578c4d39a16f6b37b
CWJWANJING <wanjingchencwj@gmail.com>
2020-05-14 18:00:42 +0100

    Update tic-tac-toe.py
    Refactorred

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -70,28 +70,28 @@ def invalid():
 
 
 def drawTheMove(x,y,dict,move):
-    if x == 1:
-        if y == 1:
+    if y == 1:
+        if x == 1:
             dict[0][1] = move
-        elif y == 2:
+        elif x == 2:
             dict[0][5] = move
-        elif y == 3:
+        elif x == 3:
             dict[0][9] = move
         action = str(x)+str(y)
-    elif x == 2:
-        if y == 1:
+    elif y == 2:
+        if x == 1:
             dict[2][1] = move
-        elif y == 2:
+        elif x == 2:
             dict[2][5] = move
-        elif y == 3:
+        elif x == 3:
             dict[2][9] = move
         action = str(x)+str(y)
-    elif x == 3:
-        if y == 1:
+    elif y == 3:
+        if x == 1:
             dict[4][1] = move
-        elif y == 2:
+        elif x == 2:
             dict[4][5] = move
-        elif y == 3:
+        elif x == 3:
             dict[4][9] = move
         action = str(x)+str(y)
     return dict, action
@@ -106,64 +106,64 @@ def switchTurns(move):
 
 def horizontal(dict):
     win = False
-    if dict[0][1] == dict[0][5] and dict[0][5] == dict[0][9] and dict[0][1] == "X":
-            print("X wins!")
-            win = True
-    elif dict[0][1] == dict[0][5] and dict[0][5] == dict[0][9] and dict[0][1] == "O":
-            print("O wins!")
-            win = True
-    elif dict[2][1] == dict[2][5] and dict[2][5] == dict[2][9] and dict[2][1] == "X":
-            print("X wins!")
-            win = True
-    elif dict[2][1] == dict[2][5] and dict[2][5] == dict[2][9] and dict[2][1] == "O":
-            print("O wins!")
-            win = True
-    elif dict[4][1] == dict[4][5] and dict[4][5] == dict[4][9] and dict[4][1] == "X":
-            print("X wins!")
-            win = True
-    elif dict[4][1] == dict[4][5] and dict[4][5] == dict[4][9] and dict[4][1] == "O":
-            print("O wins!")
-            win = True
+    if dict[0][1] == "X" and dict[0][1] == dict[0][5] and dict[0][5] == dict[0][9]:
+        print("X wins!")
+        win = True
+    elif dict[0][1] == "O" and dict[0][1] == dict[0][5] and dict[0][5] == dict[0][9]:
+        print("O wins!")
+        win = True
+    elif dict[2][1] == "X" and dict[2][1] == dict[2][5] and dict[2][5] == dict[2][9]:
+        print("X wins!")
+        win = True
+    elif dict[2][1] == "O" and dict[2][1] == dict[2][5] and dict[2][5] == dict[2][9]:
+        print("O wins!")
+        win = True
+    elif dict[4][1] == "X" and dict[4][1] == dict[4][5] and dict[4][5] == dict[4][9]:
+        print("X wins!")
+        win = True
+    elif dict[4][1] == "O" and dict[4][1] == dict[4][5] and dict[4][5] == dict[4][9]:
+        print("O wins!")
+        win = True
     return win
 
 
 def vertical(dict):
     win = False
-    if dict[0][1] == dict[2][1] and dict[2][1] == dict[4][1] and dict[0][1] == "X":
-            print("X wins!")
-            win = True
-    elif dict[0][1] == dict[2][1] and dict[2][1] == dict[4][1] and dict[0][1] == "O":
-            print("O wins!")
-            win = True
-    elif dict[0][5] == dict[2][5] and dict[2][5] == dict[4][5] and dict[0][5] == "X":
-            print("X wins!")
-            win = True
-    elif dict[0][5] == dict[2][5] and dict[2][5] == dict[4][5] and dict[0][5] == "O":
-            print("O wins!")
-            win = True
-    elif dict[0][9] == dict[2][9] and dict[2][9] == dict[4][9] and dict[0][9] == "X":
-            print("X wins!")
-            win = True
-    elif dict[0][9] == dict[2][9] and dict[2][9] == dict[4][9] and dict[0][9] == "O":
-            print("O wins!")
-            win = True
+    if dict[0][1] == "X" and dict[0][1] == dict[2][1] and dict[2][1] == dict[4][1]:
+        print("X wins!")
+        win = True
+    elif dict[0][1] == "O" and dict[0][1] == dict[2][1] and dict[2][1] == dict[4][1]:
+        print("O wins!")
+        win = True
+    elif dict[0][5] == "X" and dict[0][5] == dict[2][5] and dict[2][5] == dict[4][5]:
+        print("X wins!")
+        win = True
+    elif dict[0][5] == "O" and dict[0][5] == dict[2][5] and dict[2][5] == dict[4][5]:
+        print("O wins!")
+        win = True
+    elif dict[0][9] == "X" and dict[0][9] == dict[2][9] and dict[2][9] == dict[4][9]:
+        print("X wins!")
+        win = True
+    elif dict[0][9] == "O" and dict[0][9] == dict[2][9] and dict[2][9] == dict[4][9]:
+        print("O wins!")
+        win = True
     return win
 
 
 def cross(dict):
     win = False
-    if dict[0][1] == dict[2][5] and dict[2][5] == dict[4][9] and dict[0][1] == "X":
-            print("X wins!")
-            win = True
-    elif dict[0][1] == dict[2][5] and dict[2][5] == dict[4][9] and dict[0][1] == "O":
-            print("O wins!")
-            win = True
-    elif dict[0][9] == dict[2][5] and dict[2][5] == dict[4][1] and dict[0][1] == "X":
-            print("X wins!")
-            win = True
-    elif dict[0][9] == dict[2][5] and dict[2][5] == dict[4][1] and dict[0][1] == "O":
-            print("O wins!")
-            win = True
+    if dict[0][1] == "X" and dict[0][1] == dict[2][5] and dict[2][5] == dict[4][9]:
+        print("X wins!")
+        win = True
+    elif dict[0][1] == "O" and dict[0][1] == dict[2][5] and dict[2][5] == dict[4][9]:
+        print("O wins!")
+        win = True
+    elif dict[0][9] == "X" and dict[0][9] == dict[2][5] and dict[2][5] == dict[4][1]:
+        print("X wins!")
+        win = True
+    elif dict[0][9] == "O" and dict[0][9] == dict[2][5] and dict[2][5] == dict[4][1]:
+        print("O wins!")
+        win = True
     return win
 
 
@@ -185,8 +185,8 @@ def checkdraw(dict):
     l.append(dict[4][1])
     l.append(dict[4][5])
     l.append(dict[4][9])
+    draw = True
     for i in l:
-        draw = True
         if i == "X" or i == "O":
             draw = draw&True
         else:
@@ -196,8 +196,7 @@ def checkdraw(dict):
     return draw
 
 
-if __name__ == "__main__":
-    # draw initial stage out first
+def prep():
     dict = makeDict()
     printDict(dict)
     print("Please decide who's X or O")
@@ -206,31 +205,58 @@ if __name__ == "__main__":
     move = firstMover()
     # record the actioned moves
     moveds = []
+    return dict, move, moveds
+
+
+def game(inp, dict, move, moveds):
+    win = False
+    draw =  False
+    x = int(inp[0])
+    y = int(inp[1])
+    # take the valid move
+    dict, action = drawTheMove(x,y,dict,move)
+    moveds.append(action)
+    printDict(dict)
+    # check whether one side wins or not
+    win = checkwin(dict)
+    if win == False:
+        draw = checkdraw(dict)
+    if win == False and draw == False:
+        # switch the move
+        move = switchTurns(move)
+        print(f"{move}'s turn:")
+    return win, draw, move, moveds
+
+
+def checkinp(inp):
+    inval = False
+    # check if inputs are in the range of (1,2,3) or not
+    if inp.isdecimal() != True:
+        invalid()
+        inval = True
+    # check if the inputs are numbers
+    elif len(inp) != 2 or int(inp[0])<1 or int(inp[0])>3 or int(inp[1])<1 or int(inp[1])>3:
+        invalid()
+        inval = True
+    # check if the move has already been made before
+    elif inp in moveds:
+        print("This space has been taken, please enter a new one")
+        inval = True
+    return inval
+
+
+if __name__ == "__main__":
+    # draw initial stage out first
+    dict, move, moveds = prep()
     while True:
         # get the input, input should be in coordinates form:
         # labeling start from [1,1]
         inp = input()
-        # check if inputs are in the range of (1,2,3) or not
-        if len(inp) != 2 or int(inp[0])<1 or int(inp[0])>3 or int(inp[1])<1 or int(inp[1])>3:
-            invalid()
-        else:
-            # check if the move has already been made before
-            if inp in moveds:
-                print("This space has been taken, please enter a new one")
-            else:
-                x = int(inp[0])
-                y = int(inp[1])
-                # take the valid move
-                dict, action = drawTheMove(x,y,dict,move)
-                moveds.append(action)
-                printDict(dict)
-                # check whether one side wins or not
-                win = checkwin(dict)
-                if win == True:
-                    break
-                # if in the end no win then draw
-                if checkdraw(dict):
-                    break
-                # switch turns
-                move = switchTurns(move)
-                print(f"{move}'s turn")
+        inval = checkinp(inp)
+        if inval == False:
+            win, draw, move, moveds = game(inp, dict, move, moveds)
+            if win:
+                break
+            # if in the end no win then draw
+            if draw:
+                break
