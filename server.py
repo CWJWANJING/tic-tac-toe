ee40959330794682ae0e7c90e6fda48fe95349ef
CWJWANJING <wanjingchencwj@gmail.com>
2020-05-16 19:36:58 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ import socket
 from _thread import *
 import sys
 
-server = ""
+server = "192.168.1.213"
 port = 5555
 
 # the type of socket we want
-s = socket.socket(socket.AF_INET, socekt.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     s.bind((server, port))
@@ -19,7 +19,23 @@ s.listen(2)
 print("Server started, waiting for connections")
 
 def threaded_client(conn):
-    pass
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
 
 
 while True:
