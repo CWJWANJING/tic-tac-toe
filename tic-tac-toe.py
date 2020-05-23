ec774777383c389393f895fe1c5a6ceea1ba3277
CWJWANJING <wanjingchencwj@gmail.com>
2020-05-23 12:57:58 +0100

    Update tic-tac-toe.py
    Refactorred

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,16 +2,71 @@ import random
 import pdb
 import sys
 import socket
+ #    1   2   3
+ # a  O |   | X
+ #   -----------
+ # b    | O |
+ #   -----------
+ # c    |   |
 
- #  O |   | X
- # -----------
- #    | O |
- # -----------
- #    |   |
 
-
-def makeLine1N3N5():
+def makeLineindexes():
     line = []
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append("1")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append("2")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append("3")
+    return line
+
+
+def makeLine1():
+    line = []
+    line.append("a")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append("|")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append("|")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    return line
+
+
+def makeLine3():
+    line = []
+    line.append("b")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append("|")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    line.append("|")
+    line.append(" ")
+    line.append(" ")
+    line.append(" ")
+    return line
+
+
+def makeLine5():
+    line = []
+    line.append("c")
+    line.append(" ")
     line.append(" ")
     line.append(" ")
     line.append(" ")
@@ -28,33 +83,33 @@ def makeLine1N3N5():
 
 def makeLine2N4():
     line = []
-    line.append("-----------")
+    line.append("  -----------")
     return line
 
 
 def makeDict():
     # make a dict which stores the stages components
-    line1 = makeLine1N3N5()
-    line3 = makeLine1N3N5()
-    line5 = makeLine1N3N5()
+    line0 = makeLineindexes()
+    line1 = makeLine1()
+    line3 = makeLine3()
+    line5 = makeLine5()
 
     line2 = makeLine2N4()
     line4 = makeLine2N4()
 
     dict = {}
-    dict[0] = line1
-    dict[1] = line2
-    dict[2] = line3
-    dict[3] = line4
-    dict[4] = line5
+    dict[0] = line0
+    dict[1] = line1
+    dict[2] = line2
+    dict[3] = line3
+    dict[4] = line4
+    dict[5] = line5
     return dict
 
 
 def printDict(dict):
     for i in range(len(dict)):
         print("".join(dict[i]))
-    print("\n→ 1  2  3")
-    print("↓ 1  2  3")
 
 
 def firstMover():
@@ -72,29 +127,29 @@ def invalid():
 
 
 def drawTheMove(x,y,dict,move):
-    if y == 1:
+    if y == "a":
         if x == 1:
-            dict[0][1] = move
+            dict[1][3] = move
         elif x == 2:
-            dict[0][5] = move
+            dict[1][7] = move
         elif x == 3:
-            dict[0][9] = move
+            dict[1][11] = move
         action = str(x)+str(y)
-    elif y == 2:
+    elif y == "b":
         if x == 1:
-            dict[2][1] = move
+            dict[3][3] = move
         elif x == 2:
-            dict[2][5] = move
+            dict[3][7] = move
         elif x == 3:
-            dict[2][9] = move
+            dict[3][11] = move
         action = str(x)+str(y)
-    elif y == 3:
+    elif y == "c":
         if x == 1:
-            dict[4][1] = move
+            dict[5][3] = move
         elif x == 2:
-            dict[4][5] = move
+            dict[5][7] = move
         elif x == 3:
-            dict[4][9] = move
+            dict[5][11] = move
         action = str(x)+str(y)
     return dict, action
 
@@ -108,22 +163,22 @@ def switchTurns(move):
 
 def horizontal(dict):
     win = False
-    if dict[0][1] == "X" and dict[0][1] == dict[0][5] and dict[0][5] == dict[0][9]:
+    if dict[1][3] == "X" and dict[1][3] == dict[1][7] and dict[1][7] == dict[1][11]:
         print("X wins!")
         win = True
-    elif dict[0][1] == "O" and dict[0][1] == dict[0][5] and dict[0][5] == dict[0][9]:
+    elif dict[1][3] == "O" and dict[1][3] == dict[1][7] and dict[1][7] == dict[1][11]:
         print("O wins!")
         win = True
-    elif dict[2][1] == "X" and dict[2][1] == dict[2][5] and dict[2][5] == dict[2][9]:
+    elif dict[3][3] == "X" and dict[3][3] == dict[3][7] and dict[3][7] == dict[3][11]:
         print("X wins!")
         win = True
-    elif dict[2][1] == "O" and dict[2][1] == dict[2][5] and dict[2][5] == dict[2][9]:
+    elif dict[3][3] == "O" and dict[3][3] == dict[3][7] and dict[3][7] == dict[3][11]:
         print("O wins!")
         win = True
-    elif dict[4][1] == "X" and dict[4][1] == dict[4][5] and dict[4][5] == dict[4][9]:
+    elif dict[5][3] == "X" and dict[5][3] == dict[5][7] and dict[5][7] == dict[5][11]:
         print("X wins!")
         win = True
-    elif dict[4][1] == "O" and dict[4][1] == dict[4][5] and dict[4][5] == dict[4][9]:
+    elif dict[5][3] == "O" and dict[5][3] == dict[5][7] and dict[5][7] == dict[5][11]:
         print("O wins!")
         win = True
     return win
@@ -131,22 +186,22 @@ def horizontal(dict):
 
 def vertical(dict):
     win = False
-    if dict[0][1] == "X" and dict[0][1] == dict[2][1] and dict[2][1] == dict[4][1]:
+    if dict[1][3] == "X" and dict[1][3] == dict[3][3] and dict[3][3] == dict[5][3]:
         print("X wins!")
         win = True
-    elif dict[0][1] == "O" and dict[0][1] == dict[2][1] and dict[2][1] == dict[4][1]:
+    elif dict[1][3] == "O" and dict[1][3] == dict[3][3] and dict[3][3] == dict[5][3]:
         print("O wins!")
         win = True
-    elif dict[0][5] == "X" and dict[0][5] == dict[2][5] and dict[2][5] == dict[4][5]:
+    elif dict[1][7] == "X" and dict[1][7] == dict[3][7] and dict[3][7] == dict[5][7]:
         print("X wins!")
         win = True
-    elif dict[0][5] == "O" and dict[0][5] == dict[2][5] and dict[2][5] == dict[4][5]:
+    elif dict[1][7] == "O" and dict[1][7] == dict[3][7] and dict[3][7] == dict[5][7]:
         print("O wins!")
         win = True
-    elif dict[0][9] == "X" and dict[0][9] == dict[2][9] and dict[2][9] == dict[4][9]:
+    elif dict[1][11] == "X" and dict[1][11] == dict[3][11] and dict[3][11] == dict[5][11]:
         print("X wins!")
         win = True
-    elif dict[0][9] == "O" and dict[0][9] == dict[2][9] and dict[2][9] == dict[4][9]:
+    elif dict[1][11] == "O" and dict[1][11] == dict[3][11] and dict[3][11] == dict[5][11]:
         print("O wins!")
         win = True
     return win
@@ -154,16 +209,16 @@ def vertical(dict):
 
 def cross(dict):
     win = False
-    if dict[0][1] == "X" and dict[0][1] == dict[2][5] and dict[2][5] == dict[4][9]:
+    if dict[1][3] == "X" and dict[1][3] == dict[3][7] and dict[3][7] == dict[5][11]:
         print("X wins!")
         win = True
-    elif dict[0][1] == "O" and dict[0][1] == dict[2][5] and dict[2][5] == dict[4][9]:
+    elif dict[1][3] == "O" and dict[1][3] == dict[3][7] and dict[3][7] == dict[5][11]:
         print("O wins!")
         win = True
-    elif dict[0][9] == "X" and dict[0][9] == dict[2][5] and dict[2][5] == dict[4][1]:
+    elif dict[1][11] == "X" and dict[1][11] == dict[3][7] and dict[3][7] == dict[5][3]:
         print("X wins!")
         win = True
-    elif dict[0][9] == "O" and dict[0][9] == dict[2][5] and dict[2][5] == dict[4][1]:
+    elif dict[1][11] == "O" and dict[1][11] == dict[3][7] and dict[3][7] == dict[5][3]:
         print("O wins!")
         win = True
     return win
@@ -178,15 +233,15 @@ def checkwin(dict):
 
 def checkdraw(dict):
     l = []
-    l.append(dict[0][1])
-    l.append(dict[0][5])
-    l.append(dict[0][9])
-    l.append(dict[2][1])
-    l.append(dict[2][5])
-    l.append(dict[2][9])
-    l.append(dict[4][1])
-    l.append(dict[4][5])
-    l.append(dict[4][9])
+    l.append(dict[1][3])
+    l.append(dict[1][7])
+    l.append(dict[1][11])
+    l.append(dict[3][3])
+    l.append(dict[3][7])
+    l.append(dict[3][11])
+    l.append(dict[5][3])
+    l.append(dict[5][7])
+    l.append(dict[5][11])
     draw = True
     for i in l:
         if i == "X" or i == "O":
@@ -209,8 +264,8 @@ def prep():
 def game(inp, dict, move, moveds):
     win = False
     draw =  False
-    x = int(inp[0])
-    y = int(inp[1])
+    y = inp[0]
+    x = int(inp[1])
     # take the valid move
     dict, action = drawTheMove(x,y,dict,move)
     moveds.append(action)
@@ -226,21 +281,17 @@ def game(inp, dict, move, moveds):
     return win, draw, move, moveds
 
 
-def checkinp(inp):
-    inval = False
-    # check if inputs are in the range of (1,2,3) or not
-    if inp.isdecimal() != True:
-        invalid()
-        inval = True
-    # check if the inputs are numbers
-    elif len(inp) != 2 or int(inp[0])<1 or int(inp[0])>3 or int(inp[1])<1 or int(inp[1])>3:
-        invalid()
-        inval = True
-    # check if the move has already been made before
-    elif inp in moveds:
-        print("This space has been taken, please enter a new one")
-        inval = True
-    return inval
+def is_invalid(inp):
+    """ Returns True if input invalid, False otherwise"""
+    if inp[0].lower() not in ["a", "b", "c"]:
+        return True, "First co-ordinate must be a, b or c"
+    if len(inp) != 2:
+        return True, "You must enter two co-ords"
+    if int(inp[1]) not in [1, 2, 3]:
+        return True, "Second co-ord has to be number"
+    if inp in moveds:
+        return True, "This space has been taken, please enter a new one"
+    return False, ""
 
 def get_ip():
     ip = socket.gethostbyname(socket.gethostname())
@@ -251,18 +302,17 @@ def get_ip():
     return ip
 
 if __name__ == "__main__":
-    port = 5545
+    port = 5546
     dict, moveds = prep()
     print("X moves first")
     move = None
     whoami = None
     conn = None
     try:
-        # check if Server
+        # create a socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # if Server
         if sys.argv[1] == "server":
-        #   print IP
             ip = "0.0.0.0"
             try:
                 s.bind((ip, port))
@@ -270,7 +320,7 @@ if __name__ == "__main__":
                 print(str(e))
             s.listen(1)
             print("Server started, waiting for connections")
-        #   set up accept conn
+            # set up accept conn
             conn, addr = s.accept()
             conn.setblocking(True)
             print("Connected to:", addr)
@@ -278,9 +328,9 @@ if __name__ == "__main__":
             whoami = "X"
         # else
         else:
-        #   ask for IP
+            # ask for IP
             ip = sys.argv[1]
-        #   connect
+            # connect
             s.setblocking(True)
             s.connect((ip,port))
             move = "X"
@@ -288,49 +338,31 @@ if __name__ == "__main__":
             conn = s
         # loop
         while True:
-        #   if our_turn
+            # if our_turn
             if move == whoami:
-        #      get input
+                # get input
                 inp = input()
-                inval = checkinp(inp)
-                if inval == False:
+                inval, reason = is_invalid(inp)
+                if not inval:
                     win, draw, move, moveds = game(inp, dict, move, moveds)
-        #      send to server
                 else:
+                    print(reason)
                     continue
+                # send to server
                 conn.send(str.encode(inp))
-        #   else
+            # else
             else:
-        #      Receive
+                # Receive
                 print("Waiting for the opponent")
                 inp = conn.recv(2048).decode()
                 win, draw, move, moveds = game(inp, dict, move, moveds)
-        #   plot_move
-
-        #   check win
+            # check win
             if win:
                 break
             # if in the end no win then draw
             if draw:
                 break
-        #   update our_turn (our_turn = !our_turn)
     except Exception as e:
         print(e)
     finally:
         s.close()
-
-
-
-    # # draw initial stage out first
-    # while True:
-    #     # get the input, input should be in coordinates form:
-    #     # labeling start from 11
-    #     inp = input()
-    #     inval = checkinp(inp)
-    #     if inval == False:
-    #         win, draw, move, moveds = game(inp, dict, move, moveds)
-    #         if win:
-    #             break
-    #         # if in the end no win then draw
-    #         if draw:
-    #             break
